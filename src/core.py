d3fa8a41efdb54582bac6c85adcd2daf90b4d89f
Jake Crews <jake.crews@ramseysolutions.com>
2022-08-16 14:09:18 -0500

    Update core.py

--- src/core.py
+++ src/core.py
@@ -118,10 +118,10 @@ class TechDocsCore(BasePlugin):
         config["markdown_extensions"].append("markdown_inline_graphviz")
         config["markdown_extensions"].append("plantuml_markdown")
         
-        config["markdown_extensions"].append("mdx_truly_sane_lists")
-        config["mdx_configs"]["mdx_truly_sane_lists"] = {
-            "nested_indent": 4,
-        }
+#         config["markdown_extensions"].append("mdx_truly_sane_lists")
+#         config["mdx_configs"]["mdx_truly_sane_lists"] = {
+#             "nested_indent": 4,
+#         }
 
         # merge config supplied by user in the mkdocs.yml
         for key in mdx_configs_override:
